5f43208eb519025aa53c84007e13d6a5a3bf1508
unknown <kvitka.p@yandex.ru>
2025-01-11 13:51:02 +0300

    refactor

--- src/component/SalesTable/responseHandler.ts
+++ src/component/SalesTable/responseHandler.ts
@@ -1,4 +1,9 @@
-import { DataType, SalesAccType, SalesDataSWOTType, SalesItem } from '../../types/sales';
+import {
+    SalesAccType,
+    SalesDataType,
+    SalesExpandedData,
+    SalesItem,
+} from '../../types/sales';
 import dayjs from 'dayjs';
 import isBetween from 'dayjs/plugin/isBetween';
 import { DateType, FilterType } from '../../types/common';
@@ -6,89 +11,27 @@ import { OperationEnum, exceptionStatus } from '../../const/const';
 import { DataArticleTableType } from '../ArticleTable/type';
 dayjs.extend(isBetween);
 
-const getExpandedData = () => {};
-
 export const handlerResponseSales = (responseData: SalesItem[], datePeriod: DateType) => {
-    const savedData = localStorage.getItem('data');
-    const data: DataArticleTableType[] = savedData ? JSON.parse(savedData) : [];
-
-    const articleData = data.reduce(
-        (acc, cur) => {
-            const number = isNaN(Number(cur.costPrice)) ? undefined : Number(cur.costPrice);
-            if (number) {
-                acc[cur.article] = number;
-            }
-            return acc;
-        },
-        {} as Record<string, number>,
-    );
-
     return responseData.reduce(
-        (acc: SalesAccType, item, index) => {
-            const {
-                retail_price_withdisc_rub,
-                retail_amount,
-                commission_percent,
-                ppvz_vw,
-                delivery_rub,
-                storage_fee,
-                sale_dt,
-                deduction,
-                sa_name,
-                supplier_oper_name,
-                srid,
-                ppvz_kvw_prc,
-            } = item;
-
-            const isReturn = supplier_oper_name === OperationEnum.RETURN;
-            const isSale = supplier_oper_name === OperationEnum.SALE;
-
-            const newExpandData = {
-                key: index,
-                id: srid,
-                supplier_oper_name,
-                retail_price_withdisc_rub,
-                date: sale_dt,
-                retail_amount,
-                commission_percent,
-                ppvz_vw: ppvz_kvw_prc,
-                delivery_rub,
-                storage_fee,
-                deduction,
-                sa_name,
-                costPrice: articleData[sa_name],
-            };
+        (acc: SalesAccType, responseItem, index) => {
+            const sa_name = responseItem.sa_name;
+            const expandedData = createExpandedItem({ index, responseItem });
 
             const existingItem = acc.aggregatedData.find((item) => item.sa_name === sa_name);
 
             if (existingItem) {
-                existingItem.amountSales = isSale ? existingItem.amountSales + 1 : existingItem.amountSales;
-                existingItem.retail_price_withdisc_rub += retail_price_withdisc_rub;
-                existingItem.retail_amount += retail_amount;
-                existingItem.commission_percent += commission_percent;
-                existingItem.ppvz_vw += ppvz_kvw_prc;
-                existingItem.delivery_rub += delivery_rub;
-                existingItem.storage_fee += storage_fee;
-                existingItem.deduction += deduction;
-                existingItem.returnAmount = isReturn ? existingItem.returnAmount + 1 : existingItem.returnAmount;
-
-                existingItem.expandedData.push(newExpandData);
-            } else {
-                acc.aggregatedData.push({
-                    key: index,
-                    amountSales: isSale ? 1 : 0,
-                    returnAmount: isReturn ? 1 : 0,
-                    retail_price_withdisc_rub,
-                    retail_amount,
-                    commission_percent,
-                    ppvz_vw: ppvz_kvw_prc,
-                    delivery_rub,
-                    storage_fee,
-                    deduction,
-                    sa_name,
-                    costPrice: articleData[sa_name],
-                    expandedData: [newExpandData],
+                updateExistedItem({
+                    expandedData,
+                    item: existingItem,
+                    responseItem,
                 });
+            } else {
+                const newAggregatedItem = createNewAggregatedItem({
+                    expandedData,
+                    index,
+                    responseItem,
+                });
+                acc.aggregatedData.push(newAggregatedItem);
             }
 
             const isArticleExist = acc.supplierArticle.find((item) => item.text === sa_name);
@@ -103,4 +46,97 @@ export const handlerResponseSales = (responseData: SalesItem[], datePeriod: Date
         },
         { aggregatedData: [], supplierArticle: [] },
     );
+};
+
+const createExpandedItem = ({ responseItem, index }: { responseItem: SalesItem; index: number }) => {
+    const articleData = getArticleData();
+
+    return {
+        key: index,
+        id: responseItem.srid,
+        supplier_oper_name: responseItem.supplier_oper_name,
+        retail_price_withdisc_rub: responseItem.retail_price_withdisc_rub,
+        date: responseItem.sale_dt,
+        retail_amount: responseItem.retail_amount,
+        commission_percent: responseItem.commission_percent,
+        ppvz_vw: responseItem.ppvz_kvw_prc,
+        delivery_rub: responseItem.delivery_rub,
+        storage_fee: responseItem.storage_fee,
+        deduction: responseItem.deduction,
+        sa_name: responseItem.sa_name,
+        costPrice: articleData[responseItem.sa_name],
+    };
+};
+
+const getArticleData = () => {
+    const savedData = localStorage.getItem('data');
+
+    if (!savedData) {
+        return {};
+    }
+
+    const data: DataArticleTableType[] = JSON.parse(savedData);
+    return data.reduce((acc, cur) => {
+        const number = Number(cur.costPrice);
+        if (!isNaN(Number(cur.costPrice))) {
+            acc[cur.article] = number;
+        }
+        return acc;
+    }, {} as Record<string, number>);
+};
+
+const updateExistedItem = ({
+    expandedData,
+    item,
+    responseItem,
+}: {
+    expandedData: SalesExpandedData;
+    item: SalesDataType;
+    responseItem: SalesItem;
+}) => {
+    const { isReturn, isSale } = getOperationType(responseItem);
+
+    item.amountSales = isSale ? item.amountSales + 1 : item.amountSales;
+    item.retail_price_withdisc_rub += responseItem.retail_price_withdisc_rub;
+    item.retail_amount += responseItem.retail_amount;
+    item.commission_percent += responseItem.commission_percent;
+    item.ppvz_vw += responseItem.ppvz_kvw_prc;
+    item.delivery_rub += responseItem.delivery_rub;
+    item.storage_fee += responseItem.storage_fee;
+    item.deduction += responseItem.deduction;
+    item.returnAmount = isReturn ? item.returnAmount + 1 : item.returnAmount;
+    item.expandedData.push(expandedData);
+};
+
+const createNewAggregatedItem = ({
+    expandedData,
+    index,
+    responseItem,
+}: {
+    expandedData: SalesExpandedData;
+    index: number;
+    responseItem: SalesItem;
+}) => {
+    const { isReturn, isSale } = getOperationType(responseItem);
+    return {
+        key: index,
+        amountSales: isSale ? 1 : 0,
+        returnAmount: isReturn ? 1 : 0,
+        retail_price_withdisc_rub: responseItem.retail_price_withdisc_rub,
+        retail_amount: responseItem.retail_amount,
+        commission_percent: responseItem.commission_percent,
+        ppvz_vw: responseItem.ppvz_kvw_prc,
+        delivery_rub: responseItem.delivery_rub,
+        storage_fee: responseItem.storage_fee,
+        deduction: responseItem.deduction,
+        sa_name: responseItem.sa_name,
+        costPrice: expandedData.costPrice,
+        expandedData: [expandedData],
+    };
+};
+
+const getOperationType = (responseItem: SalesItem) => {
+    const isReturn = responseItem.supplier_oper_name === OperationEnum.RETURN;
+    const isSale = responseItem.supplier_oper_name === OperationEnum.SALE;
+    return { isReturn, isSale };
 };
